cbded5c16e5d2b7e80c71e70f492486a3c2b99f9
Melinda622 <melindasahouli@hotmail.com>
2021-11-28 13:34:10 +0100

    Hangman.java created

---
+++ src/main/java/day04/hangman/Hangman.java
@@ -0,0 +1,67 @@
+package day04.hangman;
+
+import java.util.Scanner;
+
+public class Hangman {
+    String wordToFind;
+    String status;
+    int chances;
+
+    public Hangman(String wordToFind, String status, int chances) {
+        this.wordToFind = wordToFind;
+        this.status = status;
+        this.chances = chances;
+    }
+
+    public String getWordToFind() {
+        return wordToFind;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getChances() {
+        return chances;
+    }
+
+    private void run(String wordToFind, String status, int chances) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Itt tartasz: " + status);
+            System.out.println(chances);
+            System.out.println("Mi a következő tipped?");
+            //Scanner scanner = new Scanner(System.in);
+            String guess = scanner.nextLine();
+            if (wordToFind.contains(guess)) {
+                status = updateWordFound(wordToFind, status, guess);
+                System.out.println("A találat jó!");
+            } else {
+                System.out.println("A találat nem jó!");
+                chances--;
+            }
+        }
+        while (!wordToFind.equals(status) && chances > 0);
+
+        if (wordToFind.equals(status)) {
+            System.out.println("Ügyes vagy!");
+        } else {
+            System.out.println("Sajnos nem sikerult kitalálni a szót!");
+        }
+    }
+
+    private String updateWordFound(String wordToFind, String status, String guess) {
+        char[] chars = status.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (wordToFind.charAt(i) == guess.charAt(0)) {
+                chars[i] = guess.charAt(0);
+            }
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        Hangman hangman = new Hangman("alma", "____", 8);
+        hangman.run(hangman.getWordToFind(), hangman.getStatus(), hangman.getChances());
+    }
+}
